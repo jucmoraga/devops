39ddca82c40acc3af2f8d97d16f45d8ec5215b6b
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2025-11-22 16:52:56 -0500

    Ponemos DNS de DV

--- blacklist_app/application.py
+++ blacklist_app/application.py
@@ -10,7 +10,7 @@ from app.api.api import BlacklistRegister, BlacklistHealth, BlacklistToken, Blac
 application = Flask(__name__)
 
 #Ponemos configuraciones de la app
-application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://proyectogrupo10:proyectogrupo10@terraform-20251121234952721500000001.cifuwoics1ov.us-east-1.rds.amazonaws.com:5432/proyect_db'
+application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://proyectogrupo10:proyectogrupo10@terraform-20251122212627271800000001.cifuwoics1ov.us-east-1.rds.amazonaws.com:5432/proyect_db'
 application.config['JWT_SECRET_KEY'] = 'supersecretkey'
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 application.config['PROPAGATE_EXCEPTIONS'] = True
---
+++ requests.py
@@ -0,0 +1,44 @@
+import uuid
+import random
+import requests
+
+def peticiones(url: str):
+    #Gereamos el token de acceso
+    token = requests.post(f"{url}/v1/blacklists/token").json().get('token')
+
+    #Generamos tipo de request (exitoso/malo)
+    tipo_request = random.choice(['exitoso', 'malo'])
+
+    if tipo_request == 'exitoso':
+        #Generamos payload
+        payload = {
+            "email": f"user_{uuid.uuid4()}@example.com",
+            "appId": str(uuid.uuid4()),
+            "blockedReason": "Razón de prueba"
+        }
+
+        #Definimos headers
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {token}'
+        }
+
+        #Realizamos la peticion POST para agregar a la blacklist
+        requests.post(f"{url}/v1/blacklists", json = payload, headers = headers)
+
+        #Realizamos la peticion GET para consultar el email agregado
+        requests.get(f"{url}/v1/blacklists/{payload.get('email')}", headers = headers)
+    
+    else:
+        pass
+
+if __name__ == '__main__':
+    #URL localhost
+    url_local = 'http://localhost:5000'
+
+    #URL del balanceador de carga
+    url_cloud = 'http://elb-1891227252.us-east-1.elb.amazonaws.com'
+
+    #Prueba de peticiones
+    for _ in range(1000):
+        peticiones(url_local)
